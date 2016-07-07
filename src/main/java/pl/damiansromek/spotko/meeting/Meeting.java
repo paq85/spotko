6d497e9698a2f79fb4ee8219c2f45393ffed7fea
paq85 <damian.sromek@gmail.com>
2016-07-07 12:35:14 +0200

    Adding a meeting - basic. Store meetings in the DB - PostgreSQL.

--- src/main/java/pl/damiansromek/spotko/meeting/Meeting.java
+++ src/main/java/pl/damiansromek/spotko/meeting/Meeting.java
@@ -1,29 +1,57 @@
 package pl.damiansromek.spotko.meeting;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Meeting {
-	private int id;
-	
+
+	@Id
+	@org.hibernate.annotations.Type(type="pg-uuid")
+	private UUID id;
+
+	@Column(nullable = false)
 	private String name;
 	
+	@Column(nullable = false)
 	private LocalDateTime startAt;
 	
+	@Column(nullable = true)
 	private String description;
 	
-	public Meeting(int id, String name, LocalDateTime startAt)
+	public Meeting(UUID id, String name, LocalDateTime startAt)
 	{
 		this.id = id;
 		this.name = name;
 		this.startAt = startAt;
 	}
 	
-	public int getId() {
+	private Meeting() {
+	}
+	
+	public void setId(UUID id) {
+		this.id = id;
+	}
+	
+	public UUID getId() {
 		return id;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
 	}
 	
 	public String getName() {
 		return name;
+	}
+	
+	public void setStartAt(LocalDateTime startAt) {
+		this.startAt = startAt;
 	}
 	
 	public LocalDateTime getStartAt() {
--- src/main/java/pl/damiansromek/spotko/meeting/MeetingRepository.java
+++ src/main/java/pl/damiansromek/spotko/meeting/MeetingRepository.java
@@ -1,29 +1,11 @@
 package pl.damiansromek.spotko.meeting;
 
-import java.time.LocalDateTime;
-import java.util.Collection;
-import java.util.HashMap;
+import java.util.UUID;
 
-public class MeetingRepository {
-	HashMap<Integer, Meeting> meetings;
-	
-	public MeetingRepository() {
-		meetings = new HashMap<Integer, Meeting>();
-		
-		Meeting meeting = new Meeting(1, "The first meeting", LocalDateTime.of(2015, 6, 13, 20, 45));
-		meeting.setDescription("This will be a very exciting meeting.");
-		meetings.put(meeting.getId(), meeting);
-		
-		meeting = new Meeting(2, "Meeting number two", LocalDateTime.of(2015, 7, 3, 10, 15));
-		meeting.setDescription("Another cool meeting.");
-		meetings.put(meeting.getId(), meeting);
-	}
-	
-	public Collection<Meeting> findAll() {		
-		return meetings.values();
-	}
-	
-	public Meeting find(int meetingId) {
-		return meetings.get(meetingId);
-	}
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MeetingRepository extends CrudRepository<Meeting, UUID> {
+	Page<Meeting> findAll(Pageable pageable);
 }
